48331388897e7778b88f4d39b7576af82b0b147d
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2018-10-27 18:09:39 +0200

    Update
    -Eliminated giant static circle
    +Adjusted walls next to flippers

--- Pinup Pinball/ModulePhysics.cpp
+++ Pinup Pinball/ModulePhysics.cpp
@@ -38,24 +38,6 @@ bool ModulePhysics::Start()
 	b2BodyDef bd;
 	ground = world->CreateBody(&bd);
 
-	// big static circle as "ground" in the middle of the screen
-	int x = SCREEN_WIDTH / 2;
-	int y = SCREEN_HEIGHT / 1.5f;
-	int diameter = SCREEN_WIDTH / 2;
-
-	b2BodyDef body;
-	body.type = b2_staticBody;
-	body.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
-
-	b2Body* big_ball = world->CreateBody(&body);
-
-	b2CircleShape shape;
-	shape.m_radius = PIXEL_TO_METERS(diameter) * 0.5f;
-
-	b2FixtureDef fixture;
-	fixture.shape = &shape;
-	big_ball->CreateFixture(&fixture);
-
 	return true;
 }
 
